95488a526ff876a86d1f1bbd5af5a395da2660e2
Elerin Laur <elerinlaur8@gmail.com>
2021-09-19 11:14:34 +0300

    Control Flow

---
+++ UserAge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsi kasutaja vanust;
+            //programm arvutab kasutaja sünniaasta;
+            //programm kuvab sünniaasta konsoolis;
+            //"oled sündinud aastal {yearOfBirth} 
+
+            Console.WriteLine("Palun, sisesta oma vanus:");
+            int UserAge = Convert.ToInt32(Console.ReadLine());
+
+            int yearOfBirth = 2021 - UserAge;
+
+            Console.WriteLine($"Oled sündinud aastal {yearOfBirth}.");
+
+        }
+    }
+}
